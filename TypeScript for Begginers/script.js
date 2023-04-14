842128d886fea3d9f5aa13840e6f31a9d55051a8
Victor Fernandes <roviczzf@gmail.com>
2023-04-14 17:37:33 -0300

    types w/@ts-check

---
+++ TypeScript for Begginers/script.js
@@ -0,0 +1,28 @@
+//@ts-check
+const frase = "Front End";
+const total = 100.05;
+const empresas = ["Apple", "Microsoft"];
+const body = document.body;
+const button = document.querySelector("button");
+
+frase.toLowerCase();
+// frase.toFixed();
+
+total.toFixed();
+// total.toLowerCase();
+
+empresas.map((empresa) => empresa.toUpperCase());
+// empresas.toLowerCase();
+
+body.style.background = "#000";
+// body.map((item) => item);
+
+console.log(button);
+// button.click();
+
+// const operacao = true + 'teste' - (4 * {}) / [];
+const strings = "Front " + "End";
+const numbers = 100 + 200;
+console.log(strings, numbers);
+
+// Disclaimer: ao usar o @ts-check, o editor passará a indicar erros que seriam pegos pelo TS.
