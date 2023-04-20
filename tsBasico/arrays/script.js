9edde66fa95d467f91a5fc90f67930cd6b1f39b6
Victor Fernandes <roviczzf@gmail.com>
2023-04-20 18:41:49 -0300

    Arrays 2/2

--- tsBasico/arrays/script.js
+++ tsBasico/arrays/script.js
@@ -9,3 +9,31 @@ function filtrarValores(data) {
 }
 console.log(maiorQue20(numeros)); // filtrou os números maior que 20 da array "numeros".
 console.log(filtrarValores(numerosWrong)); // filtrou os números da array "numerosWrong" que haviam strings juntas.
+// Exercicio(s):
+async function fetchCursos() {
+    const response = await fetch('https://api.origamid.dev/json/cursos.json');
+    const data = await response.json();
+    mostrarCursos(data);
+}
+fetchCursos();
+function mostrarCursos(cursos) {
+    cursos.forEach(curso => {
+        let color;
+        if (curso.nivel === 'iniciante') {
+            color = 'blue';
+        }
+        else if (curso.nivel === 'avancado') {
+            color = 'red';
+        }
+        document.body.innerHTML += `
+      <div>
+        <h2 style="color: ${color};">${curso.nome}</h2>
+        <p>Horas: ${curso.horas}</p>
+        <p>Quantidade de Aulas: ${curso.aulas}</p>
+        <p>Disponibilidade: ${curso.gratuito ? 'Gratuito' : 'Pago'}</p>
+        <p>Grade: ${curso.tags.join(', ')}</p>
+        <p>Aulas: ${curso.idAulas.join(' | ')}</p>
+      </div>
+    `;
+    });
+}
--- tsBasico/arrays/script.ts
+++ tsBasico/arrays/script.ts
@@ -11,3 +11,46 @@ function filtrarValores(data: (string | number)[]) {
 
 console.log(maiorQue20(numeros)); // filtrou os números maior que 20 da array "numeros".
 console.log(filtrarValores(numerosWrong)); // filtrou os números da array "numerosWrong" que haviam strings juntas.
+
+// Exercicio(s):
+
+async function fetchCursos() {
+  const response = await fetch('https://api.origamid.dev/json/cursos.json');
+  const data = await response.json();
+  mostrarCursos(data);
+}
+
+fetchCursos();
+
+interface Curso {
+  nome: string;
+  aulas: number;
+  gratuito: boolean;
+  horas: number;
+  idAulas: number[];
+  nivel: 'iniciante' | 'avancado';
+  tags: string[];
+}
+
+function mostrarCursos(cursos: Curso[]) {
+  cursos.forEach(curso => {
+    let color;
+
+    if(curso.nivel === 'iniciante') {
+      color = 'blue';
+    } else if (curso.nivel === 'avancado') {
+      color = 'red'
+    }
+
+    document.body.innerHTML += `
+      <div>
+        <h2 style="color: ${color};">${curso.nome}</h2>
+        <p>Horas: ${curso.horas}</p>
+        <p>Quantidade de Aulas: ${curso.aulas}</p>
+        <p>Disponibilidade: ${curso.gratuito ? 'Gratuito' : 'Pago'}</p>
+        <p>Grade: ${curso.tags.join(', ')}</p>
+        <p>Aulas: ${curso.idAulas.join(' | ')}</p>
+      </div>
+    `
+  })
+}
