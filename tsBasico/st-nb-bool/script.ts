59d96b6058037ad92411590dd4878a7d96e0a2ef
Victor Fernandes <roviczzf@gmail.com>
2023-04-20 15:49:31 -0300

    String, Numbers e Boolean

---
+++ tsBasico/st-nb-bool/script.ts
@@ -0,0 +1,11 @@
+const frase = 'Front-End';
+const preco = 500;
+const condi = preco > 100;
+
+if (typeof frase === 'string') {
+  console.log('Frase é uma String.')
+} else {
+  console.log('Frase não é uma String.')
+}
+
+
