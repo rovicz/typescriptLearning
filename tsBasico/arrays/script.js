4d99ea640b43a03371d0e6c0fa7620c6223a5a62
Victor Fernandes <roviczzf@gmail.com>
2023-04-20 18:30:07 -0300

    arrays 1/2

---
+++ tsBasico/arrays/script.js
@@ -0,0 +1,11 @@
+"use strict";
+const numeros = [2, 4, 8, 16, 32, 64, 128]; // numeros: number, já que só possui número(s) na array.
+const numerosWrong = [5, 10, 100, '1000']; // numerosWrongs: number | string, já QUE possui número(s) e string(s) na array.
+function maiorQue20(data) {
+    return data.filter((n) => n > 20);
+}
+function filtrarValores(data) {
+    return data.filter(item => typeof item === 'number');
+}
+console.log(maiorQue20(numeros)); // filtrou os números maior que 20 da array "numeros".
+console.log(filtrarValores(numerosWrong)); // filtrou os números da array "numerosWrong" que haviam strings juntas.
