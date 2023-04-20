8a6651687c40ec8895c6fe6d6760605e6e14dc18
Victor Fernandes <roviczzf@gmail.com>
2023-04-20 16:47:48 -0300

    Union & Types 2/2

---
+++ tsBasico/union-types/script.js
@@ -0,0 +1,33 @@
+"use strict";
+let total = 200;
+total = '5000'; // funciona, já que defini que total poderia ser tanto uma string quanto um número.
+function isNumber(value) {
+    if (typeof value === 'number') {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+console.log(isNumber('200')); // false.
+// DOM 
+const button = document.querySelector('button');
+button?.click(); // o "?" faz com que só execute o click no button caso o button seja diferente de null ou undefined. 
+// Exercicio(s)
+// 1 - Crie uma função chamada toNumber
+// 2 - A função pode receber number | string
+// 3 - Se a função receber um número, retorne um número
+// 4 - Se a função receber uma string, retorne um número
+// 5 - Se ela receber algo diferente, retorne um erro. (throw "value deve ser um número ou uma string")
+function toNumber(value) {
+    if (typeof value === 'number') {
+        return value;
+    }
+    else if (typeof value === 'string') {
+        return Number(value);
+    }
+    else {
+        throw 'value deve ser um número ou uma string';
+    }
+}
+console.log(toNumber('500')); // retorna 500 em número.
--- tsBasico/union-types/script.ts
+++ tsBasico/union-types/script.ts
@@ -16,3 +16,23 @@ console.log(isNumber('200')) // false.
 const button = document.querySelector('button');
 
 button?.click(); // o "?" faz com que só execute o click no button caso o button seja diferente de null ou undefined. 
+
+// Exercicio(s)
+
+// 1 - Crie uma função chamada toNumber
+// 2 - A função pode receber number | string
+// 3 - Se a função receber um número, retorne um número
+// 4 - Se a função receber uma string, retorne um número
+// 5 - Se ela receber algo diferente, retorne um erro. (throw "value deve ser um número ou uma string")
+
+function toNumber(value: string | number) {
+  if (typeof value === 'number') {
+    return value;
+  } else if (typeof value === 'string') {
+    return Number(value)
+  } else {
+    throw 'value deve ser um número ou uma string'
+  }
+}
+
+console.log(toNumber('500')) // retorna 500 em número.
