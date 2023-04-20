bb4bc93cf708727198111865097a7ff51d003963
Victor Fernandes <roviczzf@gmail.com>
2023-04-20 18:19:19 -0300

    Types & Interfaces 3/3

--- tsBasico/types-inter/script.js
+++ tsBasico/types-inter/script.js
@@ -1,4 +1,5 @@
 "use strict";
+// Type
 function preencherDados(dados) {
     document.body.innerHTML += `
       <div>
@@ -20,3 +21,19 @@ const notebook = {
 };
 preencherDados(computador);
 preencherDados(notebook);
+// Exercicio(s):
+async function fetchProduct() {
+    const response = await fetch('https://api.origamid.dev/json/notebook.json');
+    const data = await response.json();
+    showProduct(data);
+}
+fetchProduct();
+function showProduct(data) {
+    document.body.innerHTML = `
+    <div>
+      <h2>${data.nome}</h2>
+      <p>R$ ${data.preco}</p>
+      <p>Descrição do Produto: ${data.descricao}</p>
+    </div>
+  `;
+}
--- tsBasico/types-inter/script.ts
+++ tsBasico/types-inter/script.ts
@@ -1,10 +1,12 @@
-type Produto = {
+// Type
+
+type ProdutoType = {
   nome: string;
   preco: number;
   teclado: boolean;
 }
 
-function preencherDados(dados: Produto) {
+function preencherDados(dados: ProdutoInter) {
   document.body.innerHTML += `
       <div>
         <h2>${dados.nome}</h2>
@@ -14,13 +16,13 @@ function preencherDados(dados: Produto) {
   `;
 }
 
-const computador: Produto = {
+const computador: ProdutoType = {
   nome: 'Computador',
   preco: 5000,
   teclado: true,
 }
 
-const notebook: Produto = {
+const notebook: ProdutoInter = {
   nome: 'Notebook',
   preco: 3000,
   teclado: false,
@@ -28,3 +30,37 @@ const notebook: Produto = {
 
 preencherDados(computador);
 preencherDados(notebook);
+
+// Interface
+
+interface ProdutoInter {
+  nome: string;
+  preco: number;
+  teclado: boolean;
+} // diferente do Type acima de "Produto", o Interface deve-se ser utilizado na criação de objetos, sendo assim, a maneira aconselhada de se utilizar é a exemplificada pelo "`ProdutoInter".
+
+// Exercicio(s):
+
+async function fetchProduct() {
+  const response = await fetch('https://api.origamid.dev/json/notebook.json');
+  const data = await response.json();
+  showProduct(data);
+}
+
+fetchProduct();
+
+interface ProdutosExer {
+  nome: string;
+  preco: number;
+  descricao: string;
+}
+
+function showProduct(data: ProdutosExer) {
+  document.body.innerHTML = `
+    <div>
+      <h2>${data.nome}</h2>
+      <p>R$ ${data.preco}</p>
+      <p>Descrição do Produto: ${data.descricao}</p>
+    </div>
+  `;
+}
