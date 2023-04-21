caa8beaf7228d073b0f214ee4fc4f76d3c8245eb
Victor Fernandes <roviczzf@gmail.com>
2023-04-20 22:28:29 -0300

    any, null & undefined 3/3

--- tsBasico/any-null-undef/script.ts
+++ tsBasico/any-null-undef/script.ts
@@ -31,3 +31,13 @@ if (button) button.click();
 button?.click();
 
 console.log(typeof null); // null é um tipo primitivo que representa a ausência de valor. É comum em funções do DOM que fazem uma busca, retornarem null quando não são bem sucedidas.
+
+// Undefined
+
+let total;
+console.log(total); // undefined
+
+// undefined representa variáveis/propriedades que foram instanciadas, porém, os seus valores ainda não foram definidos.
+
+const data = {};
+// console.log(data.nome);
