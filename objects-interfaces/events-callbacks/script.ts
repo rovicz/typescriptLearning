883757f1d9d0f533496f279ab9a410005454b13c
Victor Fernandes <roviczzf@gmail.com>
2023-04-26 18:37:19 -0300

    events & callback 3/3

--- objects-interfaces/events-callbacks/script.ts
+++ objects-interfaces/events-callbacks/script.ts
@@ -27,3 +27,26 @@ function handleClick2(this: HTMLButtonElement, event: Event) { // pode se sacrif
 }
 
 button2?.addEventListener('click', handleClick2); 
+
+// Atividade:
+
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event: PointerEvent) {
+  const button = event.currentTarget;
+  const nav = document.getElementById('nav');
+  if (button instanceof HTMLElement && nav) {
+    const active = nav.classList.contains('active');
+    if (active) {
+      nav.classList.remove('active');
+      button.setAttribute('aria-expanded', 'false');
+      button.setAttribute('aria-label', 'Abrir Menu');
+    } else {
+      nav.classList.add('active');
+      button.setAttribute('aria-expanded', 'true');
+      button.setAttribute('aria-label', 'Fechar Menu');
+    }
+  }
+}
+
+btnMobile?.addEventListener('pointerdown', toggleMenu);
