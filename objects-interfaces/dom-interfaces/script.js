b02e869bfbd962f5becfc7d7286db87200a6b78f
Victor Fernandes <roviczzf@gmail.com>
2023-04-26 15:28:10 -0300

    DOM-Interfaces 2/3

---
+++ objects-interfaces/dom-interfaces/script.js
@@ -0,0 +1,16 @@
+"use strict";
+// o querySelector retorna um objeto dependente da string que passarmos no método, como por exemplo:
+const img = document.querySelector('img'); // HTMLImageElement.
+const video = document.querySelector('video'); // HTMLVideoElement.
+// pode se aplicar o uso de instanceof para verificar constantes em caso de captura por id, por exemplo:
+const videoID = document.querySelector('videoprincipal');
+if (videoID instanceof HTMLVideoElement) {
+    videoID.volume; // Retorna 1.
+} // normalmente, videoID é reconhecido como um Element, porém ao verificar se ele é uma instância de HTMLVideoElement, ele retorna true e atribui os seus métodos.
+// querySelectorAll
+const links = document.querySelectorAll('.link'); // Retorna uma NodeList de elementos.
+links.forEach((link) => {
+    if (link instanceof HTMLAnchorElement) {
+        return link.href;
+    }
+});
--- objects-interfaces/dom-interfaces/script.ts
+++ objects-interfaces/dom-interfaces/script.ts
@@ -10,3 +10,13 @@ const videoID = document.querySelector('videoprincipal');
 if (videoID instanceof HTMLVideoElement) {
   videoID.volume; // Retorna 1.
 } // normalmente, videoID é reconhecido como um Element, porém ao verificar se ele é uma instância de HTMLVideoElement, ele retorna true e atribui os seus métodos.
+
+// querySelectorAll
+
+const links = document.querySelectorAll('.link'); // Retorna uma NodeList de elementos.
+
+links.forEach((link) => {
+  if (link instanceof HTMLAnchorElement) {
+    return link.href;
+  }
+})
