b3e4a3c258260a2dddb2bc4122586df21310050a
Victor Fernandes <roviczzf@gmail.com>
2023-05-05 10:55:08 -0300

    User Type Guard 2/3

--- objects-interfaces/usertypeguard/script.js
+++ objects-interfaces/usertypeguard/script.js
@@ -23,3 +23,23 @@ function handleData(data) {
     }
 }
 handleData(200.50); // 201.
+// Objetos
+async function fetchProduto() {
+    const response = await fetch('https://api.origamid.dev/json/notebook.json');
+    const json = await response.json();
+    handleProduto(json);
+}
+fetchProduto();
+function isProduto(value) {
+    if (value && typeof value === 'object' && 'nome' in value && 'preco' in value) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function handleProduto(data) {
+    if (isProduto(data)) {
+        console.log(data);
+    }
+}
--- objects-interfaces/usertypeguard/script.ts
+++ objects-interfaces/usertypeguard/script.ts
@@ -27,3 +27,32 @@ function handleData(data: unknown) {
 }
 
 handleData(200.50); // 201.
+
+// Objetos
+
+async function fetchProduto() {
+  const response = await fetch('https://api.origamid.dev/json/notebook.json');
+  const json = await response.json();
+  handleProduto(json);
+}
+
+fetchProduto();
+
+interface Produto {
+  nome: string;
+  preco: number;
+}
+
+function isProduto(value: unknown): value is Produto {
+  if (value && typeof value === 'object' && 'nome' in value && 'preco' in value) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function handleProduto(data: unknown) {
+  if (isProduto(data)) {
+    console.log(data);
+  }
+}
