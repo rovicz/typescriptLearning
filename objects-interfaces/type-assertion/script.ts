37160c0e2c1c68125f51658fe0734aa89172a632
Victor Fernandes <roviczzf@gmail.com>
2023-05-05 14:47:29 -0300

    Type Assertion 2/2

--- objects-interfaces/type-assertion/script.ts
+++ objects-interfaces/type-assertion/script.ts
@@ -46,3 +46,15 @@ videonull.volume; // erro, não existe volume de null.
 
 document.querySelector('a')!.href = 'https://www.victorferreira.dev'
 // erro de runtime.
+
+// outras sintaxes:
+
+const video1 = document.querySelector('.player') as HTMLVideoElement;
+const video2 = <HTMLVideoElement>document.querySelector('.player');
+const video3 = document.querySelector<HTMLVideoElement>('.player');
+const video4 = document.querySelector('.player');
+
+video1.volume;
+video2.volume;
+video3?.volume;
+(video4 as HTMLVideoElement).volume;
