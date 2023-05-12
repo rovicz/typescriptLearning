fb98fe5a53f3e9fd21c68057193942e196e49117
Victor Fernandes <roviczzf@gmail.com>
2023-05-12 14:01:55 -0300

    bibliotecas externas

---
+++ desenvolvimento/bibli-externas/dist/script.js
@@ -0,0 +1,5 @@
+"use strict";
+const button = $("button");
+console.log(button);
+console.log(_.intersection([2, 4, 8, 16, 32, 64], [4, 8, 16, 32, 64, 128, 356]));
+//# sourceMappingURL=script.js.map
---
+++ desenvolvimento/bibli-externas/src/script.ts
@@ -0,0 +1,7 @@
+const button = $("button");
+
+console.log(button); // retorna o button perfeitamente.
+
+console.log(
+  _.intersection([2, 4, 8, 16, 32, 64], [4, 8, 16, 32, 64, 128, 356]),
+); // verifica os similares entre as arrays, [4, 8, 16, 32, 64].
